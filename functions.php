7090a47a5ea7cf602ed9ee2b84ef71bc4806695d
phi <philipephi123@gmail.com>
2023-05-08 12:47:24 -0400

    s14c1 ajout fonction qui affiche une image sur page

--- functions.php
+++ functions.php
@@ -49,6 +49,30 @@ return $title;
 }
 add_filter('nav_menu_item_title', 'personnalisation_menu_item_title', 10, 4);
 
+/**
+ * Modifie les items du menu d'événement 
+ * pour inclure l'image en avant plan du post,
+ * la description du choix et le titre du choix
+ */
+
+function add_menu_description_and_thumbnail( $item_output, $item, $depth, $args ) {
+    if ( 'evenement' == $args->menu) {
+    // if ($args->menu == 'evenement') {
+    // if ($args-­>menu == 'evenement') {
+        $post_thumbnail_id = get_post_thumbnail_id( $item->object_id );
+        if ( $post_thumbnail_id ) {
+            $post_thumbnail_url = wp_get_attachment_image_src( $post_thumbnail_id, 'thumbnail' );
+            $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><span class="description">' . $item->description . '</span><img src="' . esc_url( $post_thumbnail_url[0] ) . '" class="menu-thumbnail" />', $item_output );
+        } else {
+            $item_output = str_replace( '">' . $args->link_before . $item->title, '">' . $args->link_before . '<span class="title">' . $item->title . '</span><span class="description">' . $item->description . '</span>', $item_output );
+        }
+    }
+    return $item_output;
+}
+add_filter( 'walker_nav_menu_start_el', 'add_menu_description_and_thumbnail', 10, 4 );
+
+
+
 
 add_theme_support( 'post-thumbnails' ); // récupère une image qu'on mit sur une article ou autre
 add_theme_support('custom-background');
