e2f97f340bbd50a3db89cdb77864d5028db07e24
phi <philipephi123@gmail.com>
2023-05-23 10:25:35 -0400

    Ajout du template-atelier

---
+++ template-parts/template-atelier.php
@@ -0,0 +1,21 @@
+<?php 
+/** 
+ * template name: Atelier
+ */
+get_header(); ?>
+<main class="site__main">
+<?php
+if ( have_posts() ) : the_post(); ?>
+<?php the_post_thumbnail('thumbnail'); ?>
+<h1><?= get_the_title(); ?></h1>
+<?php the_content();?>
+<h2><a href="<?php the_permalink(); ?>"> Les ateliers du TIM</a></h2>
+<p>Date<?php the_field('date_de_latelier'); ?></p>
+<p>Heure<?php the_field('heure_de_la_formation'); ?></p>     
+<p>Durée<?php the_field('duree'); ?></p>    
+<p>Local<?php the_field('local'); ?></p>    
+<p>Formateur<?php the_field('formateur'); ?></p>    
+<?php endif;?>
+</main><!-- #main -->
+<?php
+get_footer();
